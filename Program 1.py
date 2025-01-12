01bf5b4e39c8745dca5df5cdd286e2a145f5ca13
mansijadhav04 <mansijadhav025@gmail.com>
2025-01-12 13:48:14 +0530

    Delete Program 1.py

--- Program 1.py
+++
@@ -1,9 +0,0 @@
-# Take input from the user
-number = int(input("Enter a number: "))
-
-# Use a ternary operator to check if the number is even or odd
-result = "Even" if number % 2 == 0 else "Odd"
-
-# Output the result
-print(result)
-
