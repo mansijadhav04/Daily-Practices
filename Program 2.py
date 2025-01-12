754b1558cde23a4d322075cec463f8bf9f0bb64e
mansijadhav04 <mansijadhav025@gmail.com>
2025-01-12 13:26:15 +0530

    Add files via upload

---
+++ Program 2.py
@@ -0,0 +1,13 @@
+#Take two numbers as input from the user
+
+num1 = int(input("input the first number:"))
+num2 = int(input("input the second number:"))
+
+#Swap the number
+           
+num1, num2 = num2, num1
+
+#output the swapped numbers
+           
+print(f"After swapping: First number = {num1}, Second number = {num2}")
+
